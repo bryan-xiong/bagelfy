fbc82b0b7a484429833fd2093328b00b36795d8a
bryan-xiong <bryan.xiong234@gmail.com>
2024-07-15 14:58:10 -0700

    fixed port

--- app.py
+++ app.py
@@ -305,4 +305,5 @@ def format_duration(value):
     return f'{value:02}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
